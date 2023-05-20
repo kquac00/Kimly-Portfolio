e60536fbf6a6b756d6616ceeb9134cb5cf17657f
kquac00 <kimlyq@gmail.com>
2023-05-20 15:59:45 -0700

    update

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -14,24 +14,24 @@ const Footer = () => {
                         <div className="col mb-3">
                             <h5>Portfolio</h5>
                             <ul className="nav flex-column">
-                                <li className="nav-item mb-2"><Link to="/" className="nav-link p-0 text-body-secondary">Home</Link></li>
-                                <li className="nav-item mb-2"><Link to="/hobby" className="nav-link p-0 text-body-secondary">Hobby</Link></li>
-                                <li className="nav-item mb-2"><Link to="/history" className="nav-link p-0 text-body-secondary">History</Link></li>
+                                <li className="nav-item mb-2"><Link to="/">Home</Link></li>
+                                <li className="nav-item mb-2"><Link to="/hobby">Hobby</Link></li>
+                                <li className="nav-item mb-2"><Link to="/history">History</Link></li>
                             </ul>
                         </div>
                         <div className="col mb-3">
                             <h5>Communication</h5>
                             <ul className="nav flex-column">
-                                <li className="nav-item mb-2"><a href='https://www.linkedin.com/in/kimly-quac-aa811533/' target='_blank' rel="noopener noreferrer" className="nav-link p-0 text-body-secondary">LinkedIn</a></li>
-                                <li className="nav-item mb-2"><a href='https://github.com/kquac00' target='_blank' rel="noopener noreferrer" className="nav-link p-0 text-body-secondary">GitHub</a></li>
-                                <li className="nav-item mb-2"><Link to="/contact" className="nav-link p-0 text-body-secondary">Contact</Link></li>
+                                <li className="nav-item mb-2"><a href='https://www.linkedin.com/in/kimly-quac-aa811533/' target='_blank' rel="noopener noreferrer">LinkedIn</a></li>
+                                <li className="nav-item mb-2"><a href='https://github.com/kquac00' target='_blank' rel="noopener noreferrer">GitHub</a></li>
+                                <li className="nav-item mb-2"><Link to="/contact">Contact</Link></li>
                             </ul>
                         </div>
                         <div className="col mb-3">
                             <h5>Projects</h5>
                             <ul className="nav flex-column">
-                                <li className="nav-item mb-2"><a href='https://glittering-bavarois-1598d8.netlify.app/' target='_blank' rel="noopener noreferrer" className="nav-link p-0 text-body-secondary">Etikit</a></li>
-                                <li className="nav-item mb-2"><a href='https://statuesque-swan-894988.netlify.app/' target='_blank' rel="noopener noreferrer" className="nav-link p-0 text-body-secondary">E-Commerce</a></li>
+                                <li className="nav-item mb-2"><a href='https://glittering-bavarois-1598d8.netlify.app/' target='_blank' rel="noopener noreferrer" >Etikit</a></li>
+                                <li className="nav-item mb-2"><a href='https://statuesque-swan-894988.netlify.app/' target='_blank' rel="noopener noreferrer">E-Commerce</a></li>
                                 <li className="nav-item mb-2"><a href="/">Portfolio</a></li>
                             </ul>
                         </div>
